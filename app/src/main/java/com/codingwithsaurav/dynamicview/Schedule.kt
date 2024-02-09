f1d4e7cc241b9c01db2b36be113fbba7e9a7e50f
Saurav Kumar <sauravkumardev97@gmail.com>
2024-02-09 07:47:57 +0530

    Initial Commit

---
+++ app/src/main/java/com/codingwithsaurav/dynamicview/Schedule.kt
@@ -0,0 +1,8 @@
+package com.codingwithsaurav.dynamicview
+
+data class Schedule(
+    val scheduledDay: String = "",
+    val startTime: String = "",
+    val endTime: String = "",
+    val duration: String = ""
+)
